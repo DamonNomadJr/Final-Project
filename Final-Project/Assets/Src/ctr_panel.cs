35b0049e1d8b5b4c65fcbae2fc10c77ef576162b
Mehrzad B <mehrzadb@outlook.com>
2019-04-01 14:31:56 -0400

    Adds functionality to Profile of people and screen casting the panel

--- Final-Project/Assets/Src/ctr_panel.cs
+++ Final-Project/Assets/Src/ctr_panel.cs
@@ -1,23 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ctr_panel : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public GameObject nameField, lastnameField;
+    public int id { set; get;  }
 
     public void close()
     {
         Destroy(gameObject);
+        GameObject.FindGameObjectWithTag("MainCamera").GetComponent<ctr_controller>().deleted(id);
     }
+
+    public void set_name(string given_name)
+    {
+        this.nameField.GetComponent<Text>().text = given_name;
+    }
+    public void set_lastname(string given_last_name)
+    {
+        this.lastnameField.GetComponent<Text>().text = given_last_name;
+    }
+    public void set_avatar()
+    {
+
+    }
+
 }
